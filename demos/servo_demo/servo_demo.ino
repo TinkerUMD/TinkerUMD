955d1c31e919094a186be7f5ee1c6ec34cab2643
anishk25 <akhattar@umd.edu>
2013-09-09 11:27:20 -0400

    modified servo demo code

--- demos/servo_demo/servo_demo.ino
+++ demos/servo_demo/servo_demo.ino
@@ -7,6 +7,7 @@ const int switchPin1=2;
 const int switchPin2 = 4;
 const int ledPin = 3;
 
+// global to keep track of the servo angle
 int servoPos =0;
 
 void setup(){
@@ -19,23 +20,31 @@ void setup(){
 }
 
 void loop(){
+  
+  // rotate the servo left if switch one is pressed
   if (digitalRead(switchPin1) == HIGH){
    rotateLeft(); 
   }
+  
+  // rotate the servo right if switch two is pressed
   else if(digitalRead(switchPin2) == HIGH){
    rotateRight(); 
   }
-  Serial.println(digitalRead(switchPin1));
-  //Serial.println(digitalRead(switchPin2));
+  
   
   
 }
 
 void rotateLeft(){
   for(; servoPos < 180; servoPos++){
-   servo1.write(servoPos);     
+    
+   // writing to servo
+   servo1.write(servoPos);  
+    
+   // maping servo position to analog value between 0 and 255   
    int out = map( servoPos,0,180,0,255);
-   analogWrite(ledPin,out);
+   analogWrite(ledPin,out);   
+   
    delay(20); 
    
    if(digitalRead(switchPin1) == HIGH){
@@ -54,10 +63,15 @@ void rotateLeft(){
 
 void rotateRight(){
  for(; servoPos >= 0; servoPos--){
-   servo1.write(servoPos);
+   // writing to servo
+   servo1.write(servoPos);  
+    
+   // maping servo position to analog value between 0 and 255   
    int out = map( servoPos,0,180,0,255);
-   analogWrite(ledPin,out);
+   analogWrite(ledPin,out);   
+   
    delay(20);
+      
    if(digitalRead(switchPin1) == HIGH){
      rotateLeft();
      break;
